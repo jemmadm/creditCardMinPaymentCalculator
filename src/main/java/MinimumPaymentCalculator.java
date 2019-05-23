ee97f338e6c49eecf62b571b3babcf6141cfbfca
Jemma <jemmamillman@gmail.com>
2019-05-23 14:26:05 +0100

    methods and tests created

--- src/main/java/MinimumPaymentCalculator.java
+++ src/main/java/MinimumPaymentCalculator.java
@@ -1,2 +1,71 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class MinimumPaymentCalculator {
+    public String calculateDurationGivenMinimumPaymentCalculator(int outstandingBalance, double interestRate, double minimumPayment) {
+
+        double currentBalance = outstandingBalance + (outstandingBalance * interestRate / 100) / 12 - minimumPayment;
+        int monthsCount = 0;
+        double totalInterest = 0;
+
+        do {
+            double balance = 0;
+            balance += (currentBalance + (currentBalance * interestRate / 100) / 12) - minimumPayment;
+            totalInterest += (currentBalance * interestRate / 100) / 12;
+            currentBalance = balance;
+            monthsCount++;
+        } while (currentBalance > 0);
+
+
+        String timeToPayOffCardAndTotalInterest = "It will take " + monthsCount + " months to pay off the balance of £" + outstandingBalance + ". You will have paid £" + new DecimalFormat("##.##").format(totalInterest) + " in interest.";
+        System.out.println(timeToPayOffCardAndTotalInterest);
+        return timeToPayOffCardAndTotalInterest;
+
+    }
+
+    public String calculateDurationGivenHigherPaymentCalculator(int outstandingBalance, double interestRate, double higherMonthlyPayment){
+
+        double currentBalance = outstandingBalance + (outstandingBalance * interestRate / 100) / 12 - higherMonthlyPayment;
+        int monthsCount = 0;
+        double totalInterest = 0;
+
+        do {
+            double balance = 0;
+            balance += (currentBalance + (currentBalance * interestRate / 100) / 12) - higherMonthlyPayment;
+            totalInterest += (currentBalance * interestRate / 100) / 12;
+            currentBalance = balance;
+            monthsCount++;
+        } while (currentBalance > 0);
+
+
+        String timeToPayOffCardAndTotalInterest = "It will take " + monthsCount + " months to pay off the balance of £" + outstandingBalance + ". You will have paid £" + new DecimalFormat("##.##").format(totalInterest) + " in interest.";
+        System.out.println(timeToPayOffCardAndTotalInterest);
+        return timeToPayOffCardAndTotalInterest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Minimum Payments Credit Card Calculator!");
+        System.out.println("Calculate how long it will take for you to pay off your balance making only the minimum payment each month");
+        Scanner input = new Scanner(System.in);
+
+        try {
+            System.out.println("What is your current total balance?");
+            int outstandingBalance = input.nextInt();
+            System.out.println("What is the annual interest rate (APR) on your credit card?");
+            double interestRate = input.nextDouble();
+            System.out.println("What is the minimum monthly payment for your credit card?");
+            double minimumPayment = input.nextDouble();
+            MinimumPaymentCalculator minimumPaymentCalculator = new MinimumPaymentCalculator();
+            minimumPaymentCalculator.calculateDurationGivenMinimumPaymentCalculator(outstandingBalance, interestRate, minimumPayment);
+            System.out.println("Would you like to see the difference if you paid a regular amount above the minimum payment?");
+            String compareHigherPayment = input.next();
+            if (compareHigherPayment.equalsIgnoreCase("y")||compareHigherPayment.equalsIgnoreCase("yes")){
+                System.out.println("How much can you afford to pay each month?");
+                double higherMonthlyPayment = input.nextDouble();
+                minimumPaymentCalculator.calculateDurationGivenHigherPaymentCalculator(outstandingBalance,interestRate,higherMonthlyPayment);
+            }
+        } finally {
+            input.close();
+        }
+    }
 }
--- src/test/java/MinimumPaymentCalculatorTest.java
+++ src/test/java/MinimumPaymentCalculatorTest.java
@@ -1,5 +1,24 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinimumPaymentCalculatorTest {
+
+    MinimumPaymentCalculator minimumPaymentCalculator = new MinimumPaymentCalculator();
+
+    @Test
+    public void doesCalculateDurationGivenMinimumPaymentCalculatorWork(){
+
+        assertThat(minimumPaymentCalculator.calculateDurationGivenMinimumPaymentCalculator(1000,10,25))
+                .isEqualTo("It will take 48 months to pay off the balance of £1000. You will have paid £213.14 in interest");
+    }
+
+    @Test
+    public void doesHigherPaymentCalculatorWork(){
+
+        assertThat(minimumPaymentCalculator.calculateDurationGivenHigherPaymentCalculator(1000,10,2))
+    }
+
+
 
 }
